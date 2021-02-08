2f740d215867d456f3a4f0aa0c34eaa3bf0df0e6
maescode <56263750+maescode-bot@users.noreply.github.com>
2021-02-08 11:22:32 +0100

    add mathematics operation

--- calculator.js
+++ calculator.js
@@ -2,6 +2,24 @@ function Calculator() {
 
 }
 
-Calculator.prototype.calculate = function(param) {
-    return eval(param);
+Calculator.prototype.mathOperation = function() {
+    if (lastOperation === 'X') {
+        result = parseFloat(result) * parseFloat(dis2Num);
+    } else if (lastOperation === '+') {
+        result = parseFloat(result) + parseFloat(dis2Num);
+    } else if (lastOperation === '-') {
+        result = parseFloat(result) - parseFloat(dis2Num);
+    } else if (lastOperation === '/') {
+        result = parseFloat(result) / parseFloat(dis2Num);
+    } else if (lastOperation === '^') {
+        result = parseFloat(result) ** parseFloat(dis2Num);
+    }
+}
+
+Calculator.prototype.clearVar = function(name = '') {
+    dis1Num += dis2Num + ' ' + name + ' ';
+    display1El.innerText = dis1Num;
+    display2El.innerText = ' ';
+    dis2Num = ' ';
+    tempResultEl.innerText = result;
 }
